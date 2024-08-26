d2b08438995ad441eaf17630974830a6b2b1c081
Florencia <136658692+florchung@users.noreply.github.com>
2024-08-26 09:56:28 -0300

    Update Anuncio.hs

--- Anuncio.hs
+++ Anuncio.hs
@@ -1,21 +1,27 @@
 import Tipos
 
-data Anuncio = Anu Nombre [ Departamento ] Duracion deriving (Eq, Show, Ord)
+data Anuncio = Anu Nombre [Departamento] Duracion deriving (Eq, Show, Ord)
 
 nuevoA :: Nombre -> Duracion -> Anuncio         -- dado un nombre y una duracion en segundos retorna un nuevo Anuncio
 nuevoA nombre duracion = Anu nombre [] duracion
+
 nombreA :: Anuncio -> Nombre                    -- dado un anuncio retorna su nombre
-nombreA (Anu nombre xs duracion) = nombre
+nombreA (Anu nombre deps duracion) = nombre
+
 duracionA :: Anuncio -> Duracion                -- dado un anuncio retorna su duración
-duracionA (Anu nombre xs duracion) = duracion
-departamentosA :: Anuncio -> [ Departamento ]   -- dado un anuncio retorna los departamentos que le fueron asociados
-departamentosA (Anu nombre xs duracion) = xs
+duracionA (Anu nombre deps duracion) = duracion
+
+departamentosA :: Anuncio -> [Departamento]   -- dado un anuncio retorna los departamentos que le fueron asociados
+departamentosA (Anu nombre deps duracion) = deps
+
 agregarA :: Departamento -> Anuncio -> Anuncio -- permite asignar un departamento a un anuncio
-agregarA departamento (Anu nombre xs duracion) = (Anu nombre (xs++ys) duracion)
+agregarA departamento (Anu nombre deps duracion) = (Anu nombre (deps++ys) duracion)
                                                   where ys = [y|y <- [departamento] , (elem departamento xs)==False]
+
 sacarA :: Departamento -> Anuncio -> Anuncio    -- permite quitarle un departamento a un anuncio
-sacarA departamento (Anu nombre xs duracion) = (Anu nombre ys duracion)
-                                               where ys = [x|x<-xs, x/=departamento]
-aplicaA :: [ Departamento ] -> Anuncio -> Bool  -- responde si un anuncion debe emitirse para alguno de los departamentos consultados
-aplicaA xs (Anu nombre ys duracion) = (length zs) == (length xs)
-     where zs = [z|z<-xs,(elem z ys)==True]
+sacarA departamento (Anu nombre deps duracion) = (Anu nombre ys duracion)
+                                               where ys = [x|x<-deps, x/=departamento]
+
+aplicaA :: [Departamento] -> Anuncio -> Bool  -- responde si un anuncion debe emitirse para alguno de los departamentos consultados
+aplicaA deps (Anu nombre ys duracion) = (length zs) == (length deps)
+     where zs = [z|z<-deps,(elem z ys)==True]
