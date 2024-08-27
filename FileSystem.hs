b4998f893336c0414d84f04448c7ecb3933c620f
Florencia <136658692+florchung@users.noreply.github.com>
2024-08-27 09:53:39 -0300

    Update FileSystem.hs

--- FileSystem.hs
+++ FileSystem.hs
@@ -17,19 +17,33 @@ departamentosF (FS [departamento] [anuncio]) = [departamento]
 anunciosF :: FileSystem -> [ Anuncio ]                            -- dado un FileSystem retorna los anuncios que incluye
 anunciosF (FS [departamento] [anuncio]) = [anuncio]
 
-agregarAnuncioF :: Anuncio -> FileSystem -> FileSystem            -- permite agregar un anuncio  
-agregarAnuncioF anuncio (FS _ anuncios) | (elem anuncio anuncios) == False = (FS _ (anuncio:anuncios))
-                                        | True = (FS _ anuncios)
+agregarAnuncioF :: Anuncio -> FileSystem -> FileSystem
+agregarAnuncioF (Anu nombre anuncioDeps duracion) (FS departamentos anuncios)
+  | any (`notElem` departamentos) anuncioDeps = error "Uno o más departamentos del anuncio no están en el FileSystem"
+  | Anu nombre anuncioDeps duracion `elem` anuncios = error "El anuncio ya está en el FileSystem"
+  | otherwise = FS (foldr (\dep acc -> if dep `elem` acc then acc else dep:acc) departamentos anuncioDeps) (Anu nombre anuncioDeps duracion : anuncios)
 
-sacarAnuncioF :: Anuncio -> FileSystem -> FileSystem              -- permite eliminar un anuncio
-sacarAnuncioF anuncio (FS _ anuncios) = (FS _ anuncios2)
-                                               where anuncios2 = [anun|anun<-anuncios, anun/=anuncio]
+sacarAnuncioF :: Anuncio -> FileSystem -> FileSystem
+sacarAnuncioF anuncio (FS departamentos anuncios)
+  | not (elem anuncio anuncios) = error "El anuncio no existe en el FileSystem"
+  | otherwise = FS departamentos [anun | anun <- anuncios, anun /= anuncio]
               
-agregarDepartamentoF :: Departamento -> FileSystem -> FileSystem  -- permite agregar un departamento
-agregarDepartamentoF depto (FS departamentos anuncios) 
+agregarDepartamentoF :: Departamento -> FileSystem -> FileSystem
+agregarDepartamentoF depto (FS departamentos anuncios)
+  | depto `elem` departamentos = error "El departamento ya existe en el FileSystem"
+  | otherwise = FS (depto:departamentos) anuncios
 
-sacarDepartamentoF :: Departamento -> FileSystem -> FileSystem    -- permite eliminar un departamento
-sacarDepartamentoF depto (FS departamentos anuncios) 
+sacarDepartamentoF :: Departamento -> FileSystem -> FileSystem
+sacarDepartamentoF depto (FS departamentos anuncios)
+  | depto `notElem` departamentos = error "El departamento no existe en el FileSystem"
+  | otherwise = FS [d | d <- departamentos, d /= depto] anuncios
 
-anunciosParaF :: [Departamento] -> FileSystem -> [Anuncio]        -- entrega los anuncios a emitir para un conjunto de departamentos
-anunciosParaF departamentos (FS _ anuncios) = [anuncio|anuncio<-anuncios, (aplicaA departamentos anuncio)]
+anunciosParaF :: [Departamento] -> FileSystem -> [Anuncio]
+anunciosParaF departamentos (FS fsDeps anuncios)
+  | null anuncios = [] -- no hay anuncios en FS, devuelve lista vacia
+  | null validDeps = error "No existen anuncios para los departamentos dados, ni otros departamentos en el sistema"
+  | otherwise = anunciosValidos -- devuelve los anuncios validos para los departamentos filtrados
+  where
+    validDeps = filter (`elem` fsDeps) departamentos
+    anunciosValidos = filter (aplicaA validDeps) anuncios
+
