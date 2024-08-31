da81560eb764fe144a2743c6ec663298a0f0f7b6
Florencia <136658692+florchung@users.noreply.github.com>
2024-08-30 23:59:12 -0300

    Update FileSystem.hs

--- FileSystem.hs
+++ FileSystem.hs
@@ -8,34 +8,40 @@ data FileSystem = FS [Departamento] [Anuncio] deriving (Eq, Show)
 b= (agregarA "jorge" (nuevoA "mi" 21))
 c= (agregarA "jorge" (nuevoA "hola" 33))
 d= (agregarA "no" (nuevoA "chau" 2))
+
+insertar ::Ord a =>  a -> [a] -> [a]
+insertar a [] = [a]
+insertar a (x:xs)
+    | a < x = a : (x : xs)
+    | a == x  = x:xs
+    | otherwise = x : insertar a xs
+
 nuevoF :: FileSystem                                              -- permite obtener un nuevo FileSystem
 nuevoF = (FS [] [] )
 
 departamentosF :: FileSystem -> [ Departamento ]                  -- dado un FileSystem retorna los departamentos que incluye
-departamentosF (FS [departamento] [anuncio]) = [departamento]
+departamentosF (FS departamento anuncio) = departamento
 
 anunciosF :: FileSystem -> [ Anuncio ]                            -- dado un FileSystem retorna los anuncios que incluye
-anunciosF (FS [departamento] [anuncio]) = [anuncio]
+anunciosF (FS departamento anuncio) = anuncio
 
 agregarAnuncioF :: Anuncio -> FileSystem -> FileSystem
-agregarAnuncioF (Anu nombre anuncioDeps duracion) (FS departamentos anuncios)
-  | any (`notElem` departamentos) anuncioDeps = error "Uno o más departamentos del anuncio no están en el FileSystem"
-  | Anu nombre anuncioDeps duracion `elem` anuncios = error "El anuncio ya está en el FileSystem"
-  | otherwise = FS (foldr (\dep acc -> if dep `elem` acc then acc else dep:acc) departamentos anuncioDeps) (Anu nombre anuncioDeps duracion : anuncios)
+agregarAnuncioF anuncio (FS departamentos anuncios ) =  FS  depts (insertar anuncio  anuncios)
+    where depts = foldr (insertar) departamentos (departamentosA anuncio)
 
 sacarAnuncioF :: Anuncio -> FileSystem -> FileSystem
 sacarAnuncioF anuncio (FS departamentos anuncios)
   | not (elem anuncio anuncios) = error "El anuncio no existe en el FileSystem"
   | otherwise = FS departamentos [anun | anun <- anuncios, anun /= anuncio]
-              
+
 agregarDepartamentoF :: Departamento -> FileSystem -> FileSystem
 agregarDepartamentoF depto (FS departamentos anuncios)
-  | depto `elem` departamentos = error "El departamento ya existe en el FileSystem"
-  | otherwise = FS (depto:departamentos) anuncios
+  | elem depto departamentos = error "El departamento ya existe en el FileSystem"
+  | otherwise = FS (insertar depto departamentos) anuncios
 
 sacarDepartamentoF :: Departamento -> FileSystem -> FileSystem
 sacarDepartamentoF depto (FS departamentos anuncios)
-  | depto `notElem` departamentos = error "El departamento no existe en el FileSystem"
+  | not (elem depto departamentos) = error "El departamento no existe en el FileSystem"
   | otherwise = FS [d | d <- departamentos, d /= depto] anuncios
 
 anunciosParaF :: [Departamento] -> FileSystem -> [Anuncio]
