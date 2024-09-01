c0d2271465cedb1bee153ef94ab5ccceff07e5d0
Florencia <136658692+florchung@users.noreply.github.com>
2024-08-31 22:54:07 -0300

    Update Test.hs

--- Test.hs
+++ Test.hs
@@ -5,6 +5,7 @@ import Prompter
 import Control.Exception
 import System.IO.Unsafe
 
+
 -- Función para verificar si una acción lanza una excepción
 testF :: Show a => a -> Bool
 testF action = unsafePerformIO $ do
@@ -29,40 +30,80 @@ p1 = nuevoP fs4
 p2 = configurarP p1 ["dep1", "dep2"]
 
 -- Pruebas de Anuncio
-tAnuncios = [ testF (nuevoA "" 0),
-              nombreA a1 == "anuncio1",
-              duracionA a1 == 10,
-              departamentosA a1 == [],
-              departamentosA (agregarA "dep1" a1) == ["dep1"],
-              departamentosA (sacarA "dep1" (agregarA "dep1" a1)) == [],
-              aplicaA ["dep1"] (agregarA "dep1" a1),
-              not (aplicaA ["dep2"] a1)
+tAnuncios = [ 
+    -- Verifica que la creacion de un anuncio con nombre vacio y duracion 0 no lanza excepcion
+    testF (nuevoA "" 0),
+
+    -- Verifica que el nombre de a1 sea 'anuncio1', es decir, si retorna bien su nombre.
+    nombreA a1 == "anuncio1",
+
+    -- Verifica que la duracion de a1 sea 10, es decir, si retorna bien su duracion.
+    duracionA a1 == 10,
+
+    -- Verifica los departamentos asociados a un anuncio, en este caso de a1
+    departamentosA a1 == [],
+
+    -- Verifica que se haya agregado bien un departamento asociado a un anuncio
+    departamentosA (agregarA "dep1" a1) == ["dep1"],
+
+    -- Verifica que se haya sacado bien un departamento de un anuncio
+    departamentosA (sacarA "dep1" (agregarA "dep1" a1)) == [],
+
+    -- Verifica que el anuncio aplique correctamente para un departamento
+    aplicaA ["dep1"] (agregarA "dep1" a1),
+    
+    -- Verifica que el anuncio no aplique para un departamento
+    not (aplicaA ["dep2"] a1)
             ]
 
--- Pruebas de FileSystem
-tFileSystem = [ testF (nuevoF), -- Verifica que no se lanza una excepción al crear un nuevo FileSystem
-                -- Test para agregar anuncios
-                departamentosF fs4 == [],
-                anunciosF fs4 == [a1, a2, a3],
-                anunciosF (sacarAnuncioF a1 fs4) == [a2, a3],
-                -- Test para agregar y sacar departamentos
-                testF (agregarDepartamentoF "dep1" fs4),
-                testF (sacarDepartamentoF "dep1" (agregarDepartamentoF "dep1" fs4)),
-                -- Test para anuncios por departamento
-                anunciosParaF ["dep1"] (agregarDepartamentoF "dep1" fs4) == [],
-                anunciosParaF [] fs4 == [a1, a2, a3]
-              ]
+tFileSystem = [
+    -- Verifica que al crear un nuevo FileSystem, inicialmente no hay departamentos
+    departamentosF (nuevoF) == [],
+
+    -- Verifica que fs4 no tiene departamentos al inicio, al mismo esto verifica la creacion del FS
+    departamentosF fs4 == [],
+
+    -- Verifica que fs4 tiene los anuncios correctos
+    anunciosF fs4 == [a1, a2, a3],
+
+    -- Verifica que después de sacar a1, quedan a2 y a3
+    anunciosF (sacarAnuncioF a1 fs4) == [a2, a3],
+
+    -- Verifica que no se lanza una excepción al agregar un departamento
+    departamentosF (agregarDepartamentoF "dep1" fs4) == ["dep1"],
+
+    -- Verifica que no se lanza una excepción al sacar un departamento
+    departamentosF (sacarDepartamentoF "dep1" (agregarDepartamentoF "dep1" fs4)) == [],
+
+    -- Verifica que no hay anuncios para "dep1"
+    anunciosParaF ["dep1"] (agregarDepartamentoF "dep1" fs4) == [],
+
+    -- Verifica que sin departamentos, se devuelven todos los anuncios
+    anunciosParaF [] fs4 == [a1, a2, a3]
+    ]
 
 -- Pruebas de Prompter
-tPrompter = [ testF (nuevoP fs4),
-              departamentosP (configurarP p1 ["dep1", "dep2"]) == ["dep1", "dep2"],
-              anunciosP p2 == ["anuncio1", "anuncio2", "anuncio3"],
-              maybe False (== a1) (showP (configurarP p1 ["dep1"])),
-              anunciosP (avanzarP p2) == ["anuncio1", "anuncio2", "anuncio3"],
-              duracionP p2 == (duracionA a1 + duracionA a2 + duracionA a3)
-            ]
+tPrompter = [ 
+    -- Verifica que no se lanza una excepción al crear un nuevo Prompter
+    departamentosP (nuevoP fs4) == [],
 
--- Ejecutar todas las pruebas y mostrar resultados
+    -- Verifica que se configuran correctamente los departamentos en el Prompter
+    departamentosP (configurarP p1 ["dep1", "dep2"]) == ["dep1", "dep2"],
+
+    -- Verifica que se obtienen correctamente los nombres de los anuncios según los departamentos configurados, en este caso con p2
+    anunciosP p2 == ["anuncio1","anuncio2","anuncio3"],
+              
+    -- Verifica que se muestra el primer anuncio cuando se configura con un solo departamento
+    maybe False (== a1) (showP (configurarP p1 ["dep1"])),
+
+    -- Verifica que al avanzar, los anuncios permanecen consistentes
+    anunciosP (avanzarP p2) == ["anuncio1","anuncio2","anuncio3"],
+
+    -- Verifica que la duración total calculada es correcta, en este caso, la suma de la duracion de los anuncios a1, a2 y a3
+    duracionP p2 == 60
+    ]
+
+-- Ejecutar todas las pruebas y mostrar resultados, imprime los resultados
 main :: IO ()
 main = do
     putStrLn "Pruebas de Anuncio:"
