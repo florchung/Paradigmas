533286c960f87c53c699a95d125b267da38b2018
Florencia <136658692+florchung@users.noreply.github.com>
2024-09-12 18:24:59 -0300

    Add files via upload

---
+++ TP1/Tipos.hs
@@ -0,0 +1,11 @@
+module Tipos
+    where
+
+-- Alias para la duración de un anuncio en segundos.
+type Duracion = Int
+
+-- Alias para representar un departamento.
+type Departamento = String
+
+-- Alias para el nombre de un anuncio.
+type Nombre = String
