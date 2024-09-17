2a659ae97aacaf92dac8438d6c21f95a77433fe7
Florencia <136658692+florchung@users.noreply.github.com>
2024-09-17 19:04:29 -0300

    Update EmptyTree.java

--- TP2/src/tree/EmptyTree.java
+++ TP2/src/tree/EmptyTree.java
@@ -1,40 +1,38 @@
 package tree;
 
-import java.util.LinkedList;
+import java.util.Collections;
 import java.util.List;
 
-class EmptyTree extends Tree {
-
-    public EmptyTree(Object carga){
-        super( null );
-    }
-    public void left(Tree b){
-        throw new RuntimeException("Nada a la siniestra!");
-    }
-    public void right(Tree a){
-        throw new RuntimeException("Nada a la diestra!");
-    }
-    public Object carga(){
-        return this;
-    }
-    private Object dfsHelper(Tree node, List result){
-        return this;
-    }
-    public List<Object> dfs(){
-        List<Object> result = new LinkedList();
-        dfsHelper(this,result);
-        return result;
-    }
-    public List<Object> bfs(){
-        List<Object> result = new LinkedList();
-        return result;
-    }
-    public Tree atLeft(Tree left){
-        return this;
-    }
-    public Tree atRight(Tree right){
-        return this;
+class EmptyTree extends TreeState {
+    public boolean isEmpty() {
+        return true;
     }
 
+    public char carga() {
+        throw new IllegalStateException("Tree is empty");
+    }
 
+    public TreeState left() {
+        throw new IllegalStateException("Tree is empty");
+    }
+
+    public TreeState right() {
+        throw new IllegalStateException("Tree is empty");
+    }
+
+    public TreeState atLeft(TreeState left) {
+        return new NonEmptyTree(' ', left, this); // Arbitrary value for root
+    }
+
+    public TreeState atRight(TreeState right) {
+        return new NonEmptyTree(' ', this, right); // Arbitrary value for root
+    }
+
+    public List<Character> dfs() {
+        return Collections.emptyList();
+    }
+
+    public List<Character> bfs() {
+        return Collections.emptyList();
+    }
 }
