8839b01d1b99a53d63e793578d91664b101e990e
Florencia <136658692+florchung@users.noreply.github.com>
2024-09-16 22:48:27 -0300

    Update EmptyTree.java

--- TP2/src/tree/EmptyTree.java
+++ TP2/src/tree/EmptyTree.java
@@ -1,12 +1,12 @@
 package tree;
 
+import java.util.LinkedList;
+import java.util.List;
+
 class EmptyTree extends Tree {
-    private Object carga = carga;
-    private Tree left = left;
-    private Tree right = right;
 
     public EmptyTree(Object carga){
-        super( carga );
+        super( null );
     }
     public void left(Tree b){
         throw new RuntimeException("Nada a la siniestra!");
@@ -15,7 +15,25 @@ class EmptyTree extends Tree {
         throw new RuntimeException("Nada a la diestra!");
     }
     public Object carga(){
-        return null;
+        return this;
+    }
+    private Object dfsHelper(Tree node, List result){
+        return this;
+    }
+    public List<Object> dfs(){
+        List<Object> result = new LinkedList();
+        dfsHelper(this,result);
+        return result;
+    }
+    public List<Object> bfs(){
+        List<Object> result = new LinkedList();
+        return result;
+    }
+    public Tree atLeft(Tree left){
+        return this;
+    }
+    public Tree atRight(Tree right){
+        return this;
     }
 
 
