f9643d2a4a4c3b878a522e166c7794f11315cd1f
ValentinLim <148253809+michasd23@users.noreply.github.com>
2024-09-20 21:20:42 -0300

    Update TreeState.java

--- TP2/src/tree/TreeState.java
+++ TP2/src/tree/TreeState.java
@@ -3,12 +3,8 @@ package tree;
 import java.util.List;
 
 public abstract class TreeState {
-    public abstract boolean isEmpty();
-    public abstract char carga();
-    public abstract TreeState left();
-    public abstract TreeState right();
-    public abstract TreeState atLeft(TreeState left);
-    public abstract TreeState atRight(TreeState right);
-    public abstract List<Character> dfs();
-    public abstract List<Character> bfs();
+    public abstract Tree left();
+    public abstract Tree right();
+
+/*    public abstract List<Object> dfs(List<Object> result);*/
 }
