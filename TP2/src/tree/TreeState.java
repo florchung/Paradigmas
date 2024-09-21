bb75ba3aa9e1a8bff06bf0defcec541bda1ebb0b
Florencia <136658692+florchung@users.noreply.github.com>
2024-09-20 23:15:04 -0300

    Update TreeState.java

--- TP2/src/tree/TreeState.java
+++ TP2/src/tree/TreeState.java
@@ -1,10 +1,11 @@
 package tree;
 
 import java.util.List;
+import java.util.Queue;
 
 public abstract class TreeState {
     public abstract Tree left();
     public abstract Tree right();
-
-/*    public abstract List<Object> dfs(List<Object> result);*/
+    public abstract void dfs(List<Object> result);
+    public abstract void enqueueChildren(Queue<Tree> queue);
 }
