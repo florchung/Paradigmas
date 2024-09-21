47745793b38b04f0d56172065f8cc048c6c28d7c
Florencia <136658692+florchung@users.noreply.github.com>
2024-09-20 23:15:41 -0300

    Update NonEmptyTree.java

--- TP2/src/tree/NonEmptyTree.java
+++ TP2/src/tree/NonEmptyTree.java
@@ -1,48 +1,28 @@
 package tree;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
 class NonEmptyTree extends TreeState {
     private Tree tree;
 
-
     public NonEmptyTree(Tree tree) {
         this.tree = tree;
-
     }
 
     public Tree left() {
-        return tree;
+        return tree.left();
     }
 
     public Tree right() {
-        return tree;
-    }
-   public List<Object> dfs(List<Object> result) {
-       result.add(left().carga());
-       return result;
+        return tree.right();
     }
 
-/*
-    public List<Object> bfs() {
-        List<Object> result = new ArrayList<>();
-        Queue<TreeState> queue = new LinkedList<>();
-        queue.add(this);
+    public void dfs(List<Object> result) {
+        tree.dfs(result);  // Delegamos la llamada DFS al nodo interno del árbol
+    }
 
-        while (!queue.isEmpty()) {
-            TreeState node = queue.poll();
-            if (node.isEmpty()) {
-                continue;
-            }
-            NonEmptyTree nonEmptyNode = (NonEmptyTree) node;
-            result.add(nonEmptyNode.carga);
-            queue.add(nonEmptyNode.left);
-            queue.add(nonEmptyNode.right);
-        }
-
-        return result;
-    }*/
+    public void enqueueChildren(Queue<Tree> queue) {
+        queue.add(tree);  // Agregamos el nodo a la cola
+    }
 }
