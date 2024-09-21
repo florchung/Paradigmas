d40d601534016c687a2d033523713b730657c54a
Florencia <136658692+florchung@users.noreply.github.com>
2024-09-20 23:14:45 -0300

    Update Tree.java

--- TP2/src/tree/Tree.java
+++ TP2/src/tree/Tree.java
@@ -1,20 +1,18 @@
-
 package tree;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Tree {
     private Object carga;
     private TreeState left = new EmptyTree();
     private TreeState right = new EmptyTree();
 
-
-
     public Tree(Object carga) {
         this.carga = carga;
     }
-
 
     public Tree atLeft(Tree left) {
         this.left = new NonEmptyTree(left);
@@ -26,7 +24,6 @@ public class Tree {
         return this;
     }
 
-
     public Object carga() {
         return carga;
     }
@@ -35,22 +32,37 @@ public class Tree {
         return left.left();
     }
 
-
     public Tree right() {
         return right.right();
     }
 
     public List<Object> dfs() {
-        List<Object> result = new ArrayList();
-        result.add(carga);
-        result.add(left.left().carga());
-        left().dfs(result);
+        List<Object> result = new ArrayList<>();
+        dfs(result);
         return result;
-        //este dfs no lo hice con emilio asi q puede estar mal
     }
 
+    protected void dfs(List<Object> result) {
+        result.add(carga);
+        left.dfs(result);
+        right.dfs(result);
+    }
 
     public List<Object> bfs() {
-        return null;
+        List<Object> result = new ArrayList<>();
+        Queue<Tree> queue = new LinkedList<>();
+        queue.add(this);
+
+        while (!queue.isEmpty()) {
+            Tree current = queue.poll();
+            result.add(current.carga());
+            current.enqueueChildren(queue);
+        }
+        return result;
+    }
+
+    private void enqueueChildren(Queue<Tree> queue) {
+        left.enqueueChildren(queue);
+        right.enqueueChildren(queue);
     }
 }
