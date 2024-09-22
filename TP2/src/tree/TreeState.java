3746312dc41acfe0b09cfd40981838509645cfc4
ValentinLim <148253809+michasd23@users.noreply.github.com>
2024-09-22 09:25:41 -0300

    Update TreeState.java

--- TP2/src/tree/TreeState.java
+++ TP2/src/tree/TreeState.java
@@ -3,8 +3,8 @@ package tree;
 import java.util.*;
 
 public abstract class TreeState {
-    public abstract Tree getLeftOrThrow();
-    public abstract Tree getRightOrThrow();
+    public abstract Tree left();
+    public abstract Tree right();
     public abstract List<Object> dfs();
     public abstract void addToQueue(Queue<Tree> queue);
 }
