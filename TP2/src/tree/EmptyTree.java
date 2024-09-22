b5f9d698206b3a6a0c2bad37b89fecadf156af5e
Florencia <136658692+florchung@users.noreply.github.com>
2024-09-22 00:12:53 -0300

    Update EmptyTree.java

--- TP2/src/tree/EmptyTree.java
+++ TP2/src/tree/EmptyTree.java
@@ -3,17 +3,24 @@ package tree;
 import java.util.*;
 
 public class EmptyTree extends TreeState {
-    @Override
-    public Tree getTree(String side) {
+    private final String side;
+
+    public EmptyTree(String side) {
+        this.side = side;
+    }
+
+    public Tree getLeftOrThrow() {
         throw new RuntimeException("Nada a la " + side + "!");
     }
 
-    @Override
+    public Tree getRightOrThrow() {
+        throw new RuntimeException("Nada a la " + side + "!");
+    }
+
     public List<Object> dfs() {
         return new ArrayList<>();
     }
 
-    @Override
     public void addToQueue(Queue<Tree> queue) {
         // No hay nada que agregar
     }
