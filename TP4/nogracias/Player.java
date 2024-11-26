1bef5613c0f00b4aa81eddb3787a38d64bf1537c
ValentinLim <148253809+michasd23@users.noreply.github.com>
2024-11-26 10:49:58 -0300

    Update Player.java

--- TP4/nogracias/Player.java
+++ TP4/nogracias/Player.java
@@ -1,27 +1,65 @@
-package nogracias;
+// package nogracias;
+
+// public class Player {
+//     private String name;
+//     private int points;
+
+//     public Player(String name) {
+//         this.name = name;
+//         this.points = 0;
+//     }
+
+//     public String getName() {
+//         return name;
+//     }
+
+//     public int getPoints() {
+//         return points;
+//     }
+
+//     public void addPoints(int points) {
+//         this.points += points;
+//     }
+
+//     public void resetPoints() {
+//         this.points = 0;
+//     }
+// }
+package NoGracias;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class Player {
     private String name;
-    private int points;
+    private ArrayList<Card> drawnCards = new ArrayList<>();
+    private int tokens;
+    private String EL_JUGADOR_YA_NO_TIENE_MAS_FICHAS="El jugador ya no tiene mas fichas";
 
-    public Player(String name) {
+    public Player(String name, int tokens) {
         this.name = name;
-        this.points = 0;
+        this.tokens = tokens;
     }
-
-    public String getName() {
+    public void useCoin(){
+        if(tokens==0){
+            throw new RuntimeException(EL_JUGADOR_YA_NO_TIENE_MAS_FICHAS);
+        }
+        tokens-=1;
+    }
+    public String name(){
         return name;
     }
-
-    public int getPoints() {
-        return points;
+    public int points(){
+        return -drawnCards.stream().reduce(0,(total,card)->total+card.value(), Integer ::sum)+ tokens;
     }
-
-    public void addPoints(int points) {
-        this.points += points;
+    public boolean equals(Objects anObject){
+        return Player.class.isInstance(anObject)&& name==Player.class.cast(anObject).name&&tokens==Player.class.cast(anObject).tokens;
     }
-
-    public void resetPoints() {
-        this.points = 0;
+    public void drawCard(Card aCard) {
+        drawnCards.add(aCard);
+    }
+    public void addTokens(int tokensPot) {
+        tokens+=tokensPot;
     }
 }
+
