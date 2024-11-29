cb75d0a07a0b7d3fabcc5fa57dcd8a441ff226be
Florencia <136658692+florchung@users.noreply.github.com>
2024-11-28 23:13:08 -0300

    Update Card.java

--- TP4/src/nothanks/Card.java
+++ TP4/src/nothanks/Card.java
@@ -2,12 +2,22 @@ package nothanks;
 
 public class Card {
     private int value;
+    private int tokens;
 
     public Card(int value) {
         this.value = value;
+        this.tokens = 0;
     }
 
     public int getValue() {
         return value;
+    }
+
+    public int getTokens() {
+        return tokens;
+    }
+
+    public void addToken() {
+        tokens++;
     }
 }
