34eb177262ebea8e5e493fb46369a690319f6f7c
Florencia <136658692+florchung@users.noreply.github.com>
2024-11-28 23:13:28 -0300

    Update Deck.java

--- TP4/src/nothanks/Deck.java
+++ TP4/src/nothanks/Deck.java
@@ -2,16 +2,32 @@ package nothanks;
 
 import java.util.Collections;
 import java.util.Stack;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Deck {
     private Stack<Card> cards;
 
     public Deck() {
         cards = new Stack<>();
-        for (int i = 3; i <= 35; i++) {
-            cards.push(new Card(i));
-        }
+        cards.addAll(
+                java.util.stream.IntStream.rangeClosed(3, 35)
+                        .mapToObj(Card::new)
+                        .collect(Collectors.toList())
+        );
         Collections.shuffle(cards);
+        removeRandomCards();
+    }
+
+    private void removeRandomCards() {
+        int cardsToRemove = (int) (Math.random() * 10) + 5;
+        cards = cards.stream()
+                .limit(cards.size() - cardsToRemove)
+                .collect(Collectors.toCollection(Stack::new));
+    }
+
+    public Stream<Card> getCardsStream() {
+        return cards.stream();
     }
 
     public Card drawCard() {
