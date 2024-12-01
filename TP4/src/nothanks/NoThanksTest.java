82986b3adbc5ada30c265e31c5e87c0445020517
ValentinLim <148253809+michasd23@users.noreply.github.com>
2024-12-01 20:32:05 -0300

    Update NoThanksTest.java

--- TP4/src/nothanks/NoThanksTest.java
+++ TP4/src/nothanks/NoThanksTest.java
@@ -6,77 +6,73 @@ import org.junit.jupiter.api.function.Executable;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class NoGraciasTest {
-
-    private static NoGracias game;
-    private static Player player1;
-    private static Player player2;
+    private NoGracias game;
     private ArrayList<Card> deck;
-    private List<Player> players;
 
     @BeforeEach public void setup(){
-        player1 = new Player("Jugador1",3);
-        player2= new Player("Jugador2",3);
-        players = List.of(player1,player2);
         deck = new ArrayList<>(Arrays.asList(new Card(4),new Card(5),new Card(2),new Card(21),new Card(20),new Card(3),new Card(8)));
-        game = new NoGracias(players,deck);
+        game = new NoGracias(Arrays.asList("jugador1","jugador2"),deck,3);
     }
-
-    @Test public void GameWithNoCardsNorPlayers(){
-        NoGracias game = new NoGracias(new ArrayList<>(),new ArrayList<>());
-        assertEquals(0, game.quantityOfCards());
-        assertEquals(0, game.quantityOfPlayers());
-    }
-    @Test public void QuantityOfPlayersAndCards(){
-        assertGame(2, 7, 3, 3);
+    @Test public void QuantityOfCards(){
+        assertEquals(7,game.quantityOfCards());
     }
     @Test public void DrawACard(){
-        game.draw(player1);
-        assertGame(2, 6, -1, 3);
+        game.draw("jugador1");
+        assertEquals(6,game.quantityOfCards());
+        assertEquals(-1,game.getPoints("jugador1"));
+        assertEquals(3,game.getPoints("jugador2"));
     }
     @Test public void TryToDrawACardButItIsNotTheirTurn(){
-        assertThrowsLike("No es tu turno", () -> game.draw(player2));
-        assertGame(2,7,3,3);
+        assertThrowsLike("No es tu turno", () -> game.draw("jugador2"));
+        assertEquals(7,game.quantityOfCards());
+        assertEquals(3,game.getPoints("jugador1"));
+        assertEquals(3,game.getPoints("jugador2"));
     }
     @Test public void UseToken(){
-        game.useToken(player1);
-        assertGame(2,7,2,3);
+        game.useToken("jugador1");
+        assertEquals(7,game.quantityOfCards());
+        assertEquals(2,game.getPoints("jugador1"));
+        assertEquals(3,game.getPoints("jugador2"));
     }
     @Test public void TryToUseTokenButItIsNotTheirTurn(){
-        assertThrowsLike("No es tu turno", () -> game.useToken(player2));
-        assertGame(2,7,3,3);
+        assertThrowsLike("No es tu turno", () -> game.useToken("jugador2"));
+        assertEquals(7,game.quantityOfCards());
+        assertEquals(3,game.getPoints("jugador1"));
+        assertEquals(3,game.getPoints("jugador2"));
     }
     @Test public void DrawACardWhichWasSkipped(){
-        game.useToken(player1).useToken(player2).draw(player1);
-        assertGame(2,6,0,2);
+        game.useToken("jugador1").useToken("jugador2").draw("jugador1");
+        assertEquals(6,game.quantityOfCards());
+        assertEquals(0,game.getPoints("jugador1"));
+        assertEquals(2,game.getPoints("jugador2"));;
     }
-    @Test public void DrawACardInAnEmptyDeck(){
-        game.draw(player1).draw(player1).draw(player1).draw(player1).draw(player1).draw(player1).draw(player1);
-        assertThrowsLike("Ya no hay mas cartas",()->game.draw(player1));
-    }
-    @Test public void TryToUseTokenInAnEmptyDeck(){
-        game.draw(player1).draw(player1).draw(player1).draw(player1).draw(player1).draw(player1).draw(player1);
-        assertThrowsLike("Ya no hay mas cartas",()->game.useToken(player1));
-    }
-    @Test public void TryToUseTokenButDontHaveAnyMore(){
-        game.useToken(player1).useToken(player2).useToken(player1).useToken(player2).useToken(player1).useToken(player2);
-        assertThrowsLike("El jugador ya no tiene mas fichas", ()->game.useToken(player1));
-    }
-
+//    @Test public void DrawACardInAnEmptyDeck(){
+//        game.draw("jugador1").draw("jugador1").draw("jugador1").draw("jugador1").draw("jugador1").draw("jugador1").draw("jugador1");
+//        assertThrowsLike("Ya no hay mas cartas",()->game.draw("jugador1"));
+//    }
+//    @Test public void TryToUseTokenInAnEmptyDeck(){
+//        game.draw("jugador1").draw("jugador1").draw("jugador1").draw("jugador1").draw("jugador1").draw("jugador1").draw("jugador1");
+//        assertThrowsLike("Ya no hay mas cartas",()->game.useToken("jugador1"));
+//    }
+//    @Test public void TryToUseTokenButDontHaveAnyMore(){
+//        game.useToken("jugador1").useToken("jugador2").useToken("jugador1").useToken("jugador2").useToken("jugador1").useToken("jugador2");
+//        assertThrowsLike("El jugador ya no tiene mas fichas", ()->game.useToken("jugador1"));
+//    }
+//
     private static void assertThrowsLike(String msg, Executable executable) {
         assertEquals(msg, assertThrows(RuntimeException.class, executable).getMessage());
     }
-    private static void assertGame(int quantityOfPlayers, int quantityOfCards, int player1Points, int Player2Points) {
-        assertEquals(quantityOfPlayers,game.quantityOfPlayers());
-        assertEquals(quantityOfCards,game.quantityOfCards());
-        assertEquals(player1Points,player1.points());
-        assertEquals(Player2Points,player2.points());
-    }
-
+//    private static void assertGame(int quantityOfPlayers, int quantityOfCards, int player1Points, int Player2Points) {
+//        assertEquals(quantityOfPlayers,game.quantityOfPlayers());
+//        assertEquals(quantityOfCards,game.quantityOfCards());
+//        assertEquals(player1Points,"jugador1".points());
+//        assertEquals(Player2Points,"jugador2".points());
+//    }
+//ganador perdedor
 }
