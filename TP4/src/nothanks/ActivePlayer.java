67f125d3ceca1e63c78a30c65f1285090cd99c33
ValentinLim <148253809+michasd23@users.noreply.github.com>
2024-12-01 20:33:03 -0300

    Create ActivePlayer.java

---
+++ TP4/src/nothanks/ActivePlayer.java
@@ -0,0 +1,32 @@
+package NoGracias;
+
+public class ActivePlayer extends Player{
+    private String EL_JUGADOR_YA_NO_TIENE_MAS_FICHAS="El jugador ya no tiene mas fichas";
+
+    public ActivePlayer(String name, int tokens) {
+        super(name,tokens);
+    }
+    public void useCoin(){
+        if(tokens==0){
+            throw new RuntimeException(EL_JUGADOR_YA_NO_TIENE_MAS_FICHAS);
+        }
+        tokens-=1;
+    }
+    public void drawCard(NoGracias game) {
+        if(game.deck().size()==1){
+            drawCard(game.deck().remove(0));
+            game.getPlayerInTurn(new NobodyPlays(name,points()));
+        }
+        drawnCards.add(game.deck().remove(0));
+
+    }
+
+    public int points() {
+        return -drawnCards.stream().reduce(0,(total,card)->total+card.value(), Integer ::sum)+ tokens;
+    }
+
+    public void drawCard(Card aCard) {
+        drawnCards.add(aCard);
+    }
+
+}
