174a54c406c3411278c78bf87a9c6032639fd27b
Florencia <136658692+florchung@users.noreply.github.com>
2024-12-01 22:07:17 -0300

    Update Player.java

--- TP4/src/nothanks/Player.java
+++ TP4/src/nothanks/Player.java
@@ -1,7 +1,6 @@
-package NoGracias;
+package nothanks;
 
 import java.util.ArrayList;
-
 
 public abstract class Player {
     protected Player nextPlayerInTurn;
@@ -13,20 +12,26 @@ public abstract class Player {
         this.name = name;
         this.tokens = tokens;
     }
+
     public abstract void useCoin();
+
     public String name(){
         return name;
     }
-    public abstract void drawCard(NoGracias game);
-    public Player turnPass(NoGracias noGracias) {
+
+    public abstract void drawCard(NoThanks game);
+
+    public Player turnPass(NoThanks noThanks) {
         return nextPlayerInTurn;
     }
+
     public abstract int points();
+
     public void nextPlayer(Player aPlayer){
         nextPlayerInTurn=aPlayer;
     }
+
     public void addTokens(int tokensPot){
         tokens+=tokensPot;
     }
 }
-
